197d26767515ef97431bbc8484a5daa6f6f1c373
Tahmid Faisal <tfmaher@users.noreply.github.com>
2022-05-12 21:23:19 +0600

    added dark mode

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,26 @@
-<script lang="ts">
-	import Tailwindcss from './Tailwindcss.svelte';
+<script lang="ts">import Tailwindcss from "./Tailwindcss.svelte";
 	import "../node_modules/bulma/css/bulma.css";
 	export let name: string;
+	
+	if (
+		localStorage.theme === "dark" ||
+		(!("theme" in localStorage) &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches)
+	) {
+		document.documentElement.classList.add("dark");
+	} else {
+		document.documentElement.classList.remove("dark");
+	}
+
+	// Whenever the user explicitly chooses light mode
+	const lightTheme = () => localStorage.theme = "light";
+
+	// Whenever the user explicitly chooses dark mode
+	const darkTheme = () => localStorage.theme = "dark";
+
+	// Whenever the user explicitly chooses to respect the OS preference
+	const systemTheme = () => localStorage.removeItem("theme");
+	
 </script>
 
 <Tailwindcss />
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         [...content.matchAll(/(?:class:)*([\!\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
     },
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
